c47f1b74b6a750f38b21d088835e5962ed1db1cf
chengxuan233 <111911840+chengxuan233@users.noreply.github.com>
2023-10-17 14:22:09 -0400

    Update voice_detection.py
    change api to whisper

--- voice_detection.py
+++ voice_detection.py
@@ -1,70 +1,41 @@
-import os
-import speech_recognition as sr
-from moviepy.editor import *
-from pydub import AudioSegment
-from pydub.silence import split_on_silence
+# import whisper
+
+# model = whisper.load_model("base")
+# result = model.transcribe("peppa pig.mp4")
+# print(result["text"])
+import whisper
 
 
-# Extract audio from video
-def extract_audio_from_video(video_file, audio_file):
-    video = VideoFileClip(video_file)
-    video.audio.write_audiofile(audio_file)
+def estimate_timestamps(text, total_duration):
+    sentences = text.split('. ')
+    duration_per_sentence = total_duration / len(sentences)
+    timestamps = [(i * duration_per_sentence, (i + 1) * duration_per_sentence) for i in range(len(sentences))]
+    return [
+        {"sentence": sentence.strip() + ".", "timestamp_start": start, "timestamp_end": end}
+        for sentence, (start, end) in zip(sentences, timestamps)
+    ]
 
 
-# Convert audio to text with timestamps
-def audio_to_text(audio_file):
-    r = sr.Recognizer()
+try:
+    # Load model and transcribe video
+    model = whisper.load_model("base")
+    result = model.transcribe("peppa pig.mp4")
 
-    # Use pydub to split the audio file into chunks on silence
-    audio_chunks = split_on_silence(
-        AudioSegment.from_wav(audio_file),
-        min_silence_len=500,
-        silence_thresh=-40
-    )
+    # Total duration of the video in seconds
+    total_duration = 300  # example duration - you should retrieve the actual duration
 
-    results = []
+    # Get the estimated timestamps
+    transcript = estimate_timestamps(result['text'], total_duration)
 
-    for i, chunk in enumerate(audio_chunks):
-        chunk_filename = f"chunk{i}.wav"
-        chunk.export(chunk_filename, format="wav")
+    # Save to a file
+    with open("transcription.txt", "w") as file:
+        for item in transcript:
+            sentence = item["sentence"]
+            start_time, end_time = item["timestamp_start"], item["timestamp_end"]
+            file.write(f"{start_time}s to {end_time}s: {sentence}\n")
 
-        with sr.AudioFile(chunk_filename) as source:
-            audio_listened = r.listen(source)
+except FileNotFoundError:
+    print("The specified file could not be found.")
+except Exception as e:
+    print(f"An unexpected error occurred: {str(e)}")
 
-            try:
-                # Recognize the chunk
-                text = r.recognize_google(audio_listened, show_all=True)
-
-                # Calculate start and end times
-                start_time = sum([c.duration_seconds for c in audio_chunks[:i]])
-                end_time = start_time + chunk.duration_seconds
-
-                # Append to results
-                results.append({
-                    'text': text,
-                    'start_time': start_time,
-                    'end_time': end_time
-                })
-
-            except sr.UnknownValueError:
-                print(f"Chunk {i} could not be recognized")
-            except sr.RequestError:
-                print("API unavailable")
-
-    return results
-
-
-if __name__ == "__main__":
-    output_txt_path = 'recognized_text.txt'  # Define the path for the output text file
-    video_path = 'peppa pig.mp4'
-    audio_path = 'extracted_audio.wav'
-
-    extract_audio_from_video(video_path, audio_path)
-    text_results = audio_to_text(audio_path)
-
-    with open(output_txt_path, 'w') as file:  # Open the txt file in write mode
-        for result in text_results:
-            line = f"{result['start_time']}s to {result['end_time']}s: {result['text']}\n"
-            file.write(line)  # Write each line to the txt file
-
-    print(f"Recognized text has been written to {output_txt_path}")
